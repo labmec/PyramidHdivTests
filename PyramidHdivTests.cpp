f3e46c7c0497ffc42036d92bec637d165fd452b5
Omar Duran <omaryesiduran@gmail.com>
2018-11-15 19:30:10 -0200

    Galileo

--- PyramidHdivTests.cpp
+++ PyramidHdivTests.cpp
@@ -673,8 +673,8 @@ int ComputeApproximation(TSimulationControl * sim_control)
     /// Hard code controls
     bool should_renumber_Q = true;
     bool use_pardiso_Q = true;
-    const int n_threads_error = 64;
-    const int n_threads_assembly = 64;
+    const int n_threads_error = 32;
+    const int n_threads_assembly = 32;
     bool keep_lagrangian_multiplier_Q = true;
     bool keep_matrix_Q = false;
     TPZGeoMesh *gmesh = NULL;
