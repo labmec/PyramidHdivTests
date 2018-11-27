b3e24f66f735759679404952d728ad5f7ec177b2
philippe <philippedevloo1958@gmail.com>
2018-11-27 17:21:23 -0200

    working on challenging configurations

---
+++ scripts/run_vertical_simul.sh
@@ -0,0 +1,17 @@
+# run_type geom_type h_level p_level hdiv++ draw hybrid dry_run
+
+./PyramidHdivTests 4 1 0 3 1 1 0 0 1 0
+./PyramidHdivTests 4 1 0 3 1 1 0 0 0 1
+./PyramidHdivTests 4 1 0 2 2 2 0 0 1 0
+./PyramidHdivTests 4 1 0 2 2 2 0 0 0 1
+
+./PyramidHdivTests 4 2 0 3 1 1 0 0 1 0
+./PyramidHdivTests 4 2 0 3 1 1 0 0 0 1
+./PyramidHdivTests 4 2 0 2 2 2 0 0 1 0
+./PyramidHdivTests 4 2 0 2 2 2 0 0 0 1
+
+./PyramidHdivTests 4 3 0 4 1 1 0 0 1 0
+./PyramidHdivTests 4 3 0 4 1 1 0 0 0 1
+./PyramidHdivTests 4 3 0 3 2 2 0 0 1 0
+./PyramidHdivTests 4 3 0 3 2 2 0 0 0 1
+
