653dc92fd5b2ec28c02d586650a219d59f0a0b4b
philippe <philippedevloo1958@gmail.com>
2018-11-27 18:29:48 -0200

    Included script for spherical mesh

---
+++ scripts/run_spherical_simul.sh
@@ -0,0 +1,18 @@
+# run_type geom_type h_level p_level hdiv++ draw hybrid dry_run
+
+./PyramidHdivTests 4 4 0 2 1 1 0 0 1 0
+./PyramidHdivTests 4 4 0 2 1 1 0 0 0 1
+./PyramidHdivTests 4 4 0 1 2 2 0 0 1 0
+./PyramidHdivTests 4 4 0 1 2 2 0 0 0 1
+
+./PyramidHdivTests 4 5 0 2 1 1 0 0 1 0
+./PyramidHdivTests 4 5 0 2 1 1 0 0 0 1
+./PyramidHdivTests 4 5 0 1 2 2 0 0 1 0
+./PyramidHdivTests 4 5 0 1 2 2 0 0 0 1
+
+./PyramidHdivTests 4 6 0 4 1 1 0 0 1 0
+./PyramidHdivTests 4 6 0 4 1 1 0 0 0 1
+./PyramidHdivTests 4 6 0 3 2 2 0 0 1 0
+./PyramidHdivTests 4 6 0 3 2 2 0 0 0 1
+
+
